13f63c7e415738652de6abbcd245538e6cbabcec
snorkungen <teuvo2004@gamil.com>
2023-03-22 13:41:31 +0200

    build(deps): update Cargo.lock, Cargo.toml and main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,61 @@
 /*
     I S**k
-
+    Knowing what your actions lead to is something. yes?
 
 // https://www.security.org/how-secure-is-my-password/
 
 */
 
-use std::str;
-use rand::prelude::*;
+use rand::{seq::SliceRandom, Rng};
+use rand_pcg::Pcg64;
+use rand_seeder::Seeder;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Master string used when generating a password
+    #[arg(short, long)]
+    master: String,
+
+    /// Key string used when generating a password
+    #[arg(short, long)]
+    key: String,
+}
 
 // ascii char range 33 -> 126
-const CHARS :&str = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
-const PASWORD_LENGTH: usize = 28;
+const PWD_CHARS :&str = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+const PASWORD_LENGTH: usize = 26;
 
 fn main() {
-    let master: String = "Master".to_string();
-    let key: String = "yusdggds-".to_string();
+    let args = Args::parse();
 
-    // let mut pwd = "".to_string();
-
-    let mut chars: Vec<&str> = CHARS.split("").filter(|s| s != &"").collect();
-
-    // println!("{:?}", chars);
-
-    // shuffle_chars(&mut chars,&master);
-
-    // println!("{:?}", chars);
-
-    // println!("{}", pwd)
-
-    let pwd = generate_pwd(&chars, &key);
+    let pwd_chars = generate_pwd_chars(&args.master);
+    let pwd = generate_pwd(&pwd_chars, &args.key);
+    println!("{}", pwd);
 }
 
-fn shuffle_chars(chars: &mut Vec<&str>, master: &String) {
-    panic!("Not implemented")
+fn generate_pwd_chars(master: &String) -> Vec<char> {
+    let mut chars: Vec<char> = PWD_CHARS.chars().collect();
+
+    let mut rng: Pcg64 = Seeder::from(master).make_rng();
+    chars.shuffle(&mut rng);
+
+    return chars;
 }
 
-fn generate_pwd(chars: &Vec<&str>, key: &String) -> String {
-    let mut pwd = String::new();
-    let key_vec: Vec<&str> = key.split("").collect();
+fn generate_pwd(pwd_chars: &Vec<char>, key: &String) -> String {
+    let mut pwd = String::from("");
 
-    for i in 1..PASWORD_LENGTH {
-        let mut p = i + 1;
+    let mut rng: Pcg64 = Seeder::from(key).make_rng();
+    let range = 1..=PWD_CHARS.len();
+    let start_index = rng.gen_range(range.clone());
 
-        if key_vec[p % key_vec.len()] > chars[p % chars.len()] {
-            p *= p;
-        }
-
-        pwd.push_str(chars[p % chars.len()]);
+    for _ in 0..PASWORD_LENGTH {
+        let i = start_index * rng.gen_range(range.clone());
+        pwd.push(pwd_chars[i % pwd_chars.len()]);
     }
 
-    pwd
+    return pwd;
 }
